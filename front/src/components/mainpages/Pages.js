5f09447b2dd2a69eedc908ced2aec7397c20e78e
ferdous85 <ferdous85@gmail.com>
2021-05-24 21:10:39 +0600

    frontend

--- front/src/components/mainpages/Pages.js
+++ front/src/components/mainpages/Pages.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import Products from './products/Products'
 
  function Pages() {
     return (
-        <div>
-            <h3>Pages Component</h3>
-        </div>
+        <Switch>
+            <Route path='/' exact component={Products} />
+            <Route path='/login' exact component={Login} />
+            <Route path='/register' exact component={Register} />
+            <Route path='/cart' exact component={Cart} />
+        </Switch>
     )
 }
 
