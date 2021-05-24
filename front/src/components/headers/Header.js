e29b34fcfbdd8120b9e0021fb445ea4e2960be0c
ferdous85 <ferdous85@gmail.com>
2021-05-24 20:50:31 +0600

    frontend

---
+++ front/src/components/headers/Header.js
@@ -0,0 +1,36 @@
+import React,{useState, useContext} from 'react'
+import { GlobalState } from '../../GlobalState'
+import Menu from '../headers/icon/menu.svg'
+import Close from '../headers/icon/close.svg'
+import Cart from '../headers/icon/cart.svg'
+import { Link } from 'react-router-dom'
+
+ function Header() {
+     const value = useContext(GlobalState)
+    return (
+        <header>
+            <div className="menu">
+                <img src={Menu} alt='menu' className='menu' width='30' />
+            </div>
+            <div className="logo">
+                <h1>
+                    <Link to='/' >Ecom App</Link>
+                </h1>
+            </div>
+            <ul>
+                <li><Link to='/' >Peoducts</Link></li>
+                <li><Link to='/login' >Login</Link></li>
+                <li>
+                    <img src={Close} alt="" className='menu' width="30" />
+                </li>
+            </ul>
+            <div className='cart-icon'>
+                <span>0</span>
+                <Link to='cart'>
+                <img src={Cart} alt="" width="30"  /></Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ front/src/components/mainpages/Pages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Pages() {
+    return (
+        <div>
+            <h3>Pages Component</h3>
+        </div>
+    )
+}
+
+
+export default Pages
