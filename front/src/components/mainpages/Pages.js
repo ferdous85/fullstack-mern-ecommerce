d058c98f3323c5bf5d3e85fe309e9def96aeb397
ferdous85 <ferdous85@gmail.com>
2021-05-25 11:53:18 +0600

    cng

--- front/src/components/mainpages/Pages.js
+++ front/src/components/mainpages/Pages.js
@@ -10,6 +10,7 @@ import Products from './products/Products'
     return (
         <Switch>
             <Route path='/' exact component={Products} />
+            <Route path='/detail/:id' exact component={Products} />
             <Route path='/login' exact component={Login} />
             <Route path='/register' exact component={Register} />
             <Route path='/cart' exact component={Cart} />
